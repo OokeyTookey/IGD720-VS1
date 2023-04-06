cb569a02706e6dafccadd9540a66e0e508d2e020
Anna Tookey <40760387+OokeyTookey@users.noreply.github.com>
2023-04-06 16:04:42 +0400

    - Player movement & -Animation blend trees
    - Blend trees are awesome! You can also flip animations with a properties within the animation window.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 movementInput;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
 
     public float movementSpeed;
@@ -15,21 +17,60 @@ public class PlayerController : MonoBehaviour
     public ContactFilter2D movementFilter; //For raycast layers
     List<RaycastHit2D> castedCollisions = new List<RaycastHit2D>();
 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     private void FixedUpdate()
     {
-        if (movementInput != Vector2.zero) 
+        if (movementInput != Vector2.zero)
         {
-           int count = rb.Cast(movementInput, movementFilter, castedCollisions, movementSpeed * Time.fixedDeltaTime + collsionOffset);
+            //The player cannot move diag if there is a collision in the way
+            //So, we need to check the x and y direction to see if there is a viable movement
 
-            if (count == 0)
+            bool successfulMovement = AttemptMovement(movementInput);
+            if (!successfulMovement)
             {
-                rb.MovePosition(rb.position + movementInput * movementSpeed * Time.fixedDeltaTime);
+                successfulMovement = AttemptMovement(new Vector2(movementInput.x, 0));
+                if (!successfulMovement)
+                {
+                    AttemptMovement(new Vector2(0, movementInput.y));
+                }
             }
+            animator.SetFloat("Horizontal", movementInput.x);
+            animator.SetFloat("Vertical", movementInput.y);
+            animator.SetFloat("Speed", movementInput.sqrMagnitude); //Checking the length of the movement value, is it above 0.01?
+        }
+
+        if (movementInput == Vector2.zero)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+    }
+
+    private void PlayerAnimationDirection(string AnimationName, bool TrueRFalse)
+    {
+
+    }
+
+    private bool AttemptMovement(Vector2 direction)
+    {
+        //theRay creates a new ray from the player can checks if the move is valid, aka, if the player collide with anything
+        //theRay will return a value, if the value is 0, AKA, No collsions, it will move the player.
+        int theRay = rb.Cast(direction, movementFilter, castedCollisions, movementSpeed * Time.fixedDeltaTime + collsionOffset); 
+        if (theRay == 0) //No collisions detected
+        {
+            rb.MovePosition(rb.position + direction * movementSpeed * Time.fixedDeltaTime);
+            return true; //if the movement occured
+        }
+        else
+        {
+            return false;
         }
     }
 
