4f7e0cb3f0d3baa09265b90d383ed6ed0371d1ee
Anna Tookey <40760387+OokeyTookey@users.noreply.github.com>
2023-04-16 22:09:35 +0400

    - Added page sweeper
    Small script that fades out all the text when pressed

--- Assets/_Scripts/PageSweeper.cs
+++ Assets/_Scripts/PageSweeper.cs
@@ -3,18 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using System;
 
 public class PageSweeper : MonoBehaviour
 {
-    public GameObject[] parentDialogues;
+    public GameObject parentDialogues;
     private TMP_Text[] childDialogues;
+    int index;
+    public float letterFadeOutTime = 0.6f;
 
     private void Start()
     {
-        Debug.Log("Number of Master Dialogues in the scene: " + parentDialogues.Length);
-        for (int i = 0; i < parentDialogues.Length; i++)
+        index = 0;
+
+        var children = parentDialogues.GetComponentsInChildren<TMP_Text>();
+        childDialogues = children;
+
+        foreach (var text in children)
         {
-           // childDialogues[i] = parentDialogues[i].
+            childDialogues[index]= text;
+            if (index >= childDialogues.Length)
+            {
+                break;
+            }
+            index++;
+        }
+    }
+
+    public void PageSweeperClear()
+    {
+        for (int i = 0; i < childDialogues.Length; i++)
+        {
+            StartCoroutine(FadeOut(letterFadeOutTime, childDialogues[i]));
+        }
+    }
+
+
+    public IEnumerator FadeOut(float time, TMP_Text text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / time));
+            yield return null;
         }
     }
 }
+
