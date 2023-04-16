e1edfd29d320073e2457fae40a09e0dee10ef63c
Anna Tookey <40760387+OokeyTookey@users.noreply.github.com>
2023-04-16 12:10:25 +0400

    - Changed movement system
    Decided that raycasting was silly, using very simple movement system which does the job perfectly.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -15,17 +15,6 @@ public class PlayerController : MonoBehaviour
     Vector2 movementInput;
     Vector2 currentDirectionFacing;
     Animator animator;
-    SpriteRenderer spriteRenderer;
-
-    /*public enum directionsFaced
-    {
-        up,
-        down,
-        left,
-        right
-    }*/
-
-    //public directionsFaced directions;
 
     float collsionOffset = 0.05f;
     public float movementSpeed;
@@ -35,12 +24,29 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-       // directions = directionsFaced.down;
-
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movementInput * movementSpeed;
+
+        if (movementInput != Vector2.zero) //If there is player input, this means the animation returns to the idle position.
+        {
+            animator.SetFloat("Horizontal", movementInput.x);
+            animator.SetFloat("Vertical", movementInput.y);
+            animator.SetFloat("Speed", movementInput.sqrMagnitude); //Checking the length of the movement value, is it above 0.01?
+        }
+
+        if (movementInput == Vector2.zero)
+        {
+            animator.SetFloat("Speed", 0);
+        }
+    }
+
+    //------------------------------------------------Sword related tasks.
+
 
     public void StartSwordAttack()
     {
@@ -53,100 +59,11 @@ public class PlayerController : MonoBehaviour
         swordAttack.enableAttack = false;
     }
 
-    private void FixedUpdate()
-    {
-        if (movementInput != Vector2.zero) //If there is no player input
-        {
-            //The player cannot move diag if there is a collision in the way
-            //So, we need to check the x and y direction to see if there is a viable movement
-
-            bool successfulMovement = AttemptMovement(movementInput);
-            if (!successfulMovement)
-            {
-                successfulMovement = AttemptMovement(new Vector2(movementInput.x, 0));
-                if (!successfulMovement)
-                {
-                    AttemptMovement(new Vector2(0, movementInput.y));
-                }
-            }
-            animator.SetFloat("Horizontal", movementInput.x);
-            animator.SetFloat("Vertical", movementInput.y);
-            animator.SetFloat("Speed", movementInput.sqrMagnitude); //Checking the length of the movement value, is it above 0.01?
-        }
-
-        if (movementInput == Vector2.zero)
-        {
-            animator.SetFloat("Speed", 0);
-        }
-        //Debug.Log("The player's current directions is: " + directions);
-    }
-
-    public bool AttemptMovement(Vector2 direction) //The script will first check if the player can make a viable move before actually moving
-    {
-        //theRay creates a new ray from the player can checks if the move is valid, aka, if the player collide with anything
-        //theRay will return a value, if the value is 0, AKA, No collsions, it will move the player.
-        int theRay = rb.Cast(direction, movementFilter, castedCollisions, movementSpeed * Time.fixedDeltaTime + collsionOffset);
-        if (theRay == 0) //No collisions detected
-        {
-            rb.MovePosition(rb.position + direction * movementSpeed * Time.fixedDeltaTime);
-            return true; //if the movement occured
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    /*void WhatDirectionIsThePlayerFacing()
-    {
-        if (movementInput.x > 0.01)
-        {
-            directions = directionsFaced.right;
-            //Debug.Log("Right");
-        }
-        if (movementInput.x < -0.01)
-        {
-            directions = directionsFaced.left;
-            // Debug.Log("Left");
-        }
-        if (movementInput.y > 0.01)
-        {
-            directions = directionsFaced.up;
-            // Debug.Log("Up");
-        }
-        if (movementInput.y < -0.01)
-        {
-            directions = directionsFaced.down;
-            // Debug.Log("Down");
-        }
-    }*/
-
     //------------------------------------------------Unity input system functions.
 
     void OnFire()
     {
         animator.SetTrigger("SwordAttack");
-        //TurnOnSwordCollidersBasedOnDirection();
-
-       /* switch (directions)
-        {
-            case directionsFaced.up:
-                //swordAttack.SwordAttackUp();
-                //Debug.Log("switchcase up");
-                return;
-            case directionsFaced.down:
-                //swordAttack.SwordAttackDown();
-                //Debug.Log("switchcase down");
-                return;
-            case directionsFaced.left:
-                // swordAttack.SwordAttackLeft();
-                //Debug.Log("switchcase left");
-                return;
-            case directionsFaced.right:
-                //swordAttack.SwordAttackRight();
-                //Debug.Log("switchcase right");
-                return;
-        }*/
     }
 
     void OnMove(InputValue movementValue)
@@ -154,8 +71,8 @@ public class PlayerController : MonoBehaviour
         movementInput = movementValue.Get<Vector2>();
 
         if (movementInput != Vector2.zero)
+        {
             currentDirectionFacing = movementInput;
-
-        //WhatDirectionIsThePlayerFacing(); //Could call this in update, but it makes sense when the player presses the key.
+        }
     }
 }
